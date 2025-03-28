d18799bd02082e4b9dfdb5669801ce5a00a57703
Yumineko <lucky.duet@gmail.com>
2025-03-28 22:48:00 +0900

    はじめてのコミット

---
+++ Core/OnBootInitializer.cs
@@ -0,0 +1,44 @@
+#if UNITY_EDITOR
+using Yumineko.InitializeOnBoot.Editor;
+#endif
+
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Yumineko.InitializeOnBoot
+{
+    public static class OnBootInitializer
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void InitializeOnLoad()
+        {
+#if UNITY_EDITOR
+            if (!OnBootInitializerEditorHelper.IsEnabled()) return;
+#endif
+            LoadBootScene().Forget();
+        }
+
+        private static async UniTask LoadBootScene()
+        {
+            await SceneManager.LoadSceneAsync(0);
+        }
+    }
+}
+
+#if UNITY_EDITOR
+namespace Yumineko.InitializeOnBoot.Editor
+{
+    public static class OnBootInitializerEditorHelper
+    {
+        public static bool IsEnabled()
+        {
+#if UNITY_EDITOR
+            return OnBootInitializerSettingsWindow.IsEnabled();
+#else
+            return false;
+#endif
+        }
+    }
+}
+#endif
---
+++ Editor/OnBootInitializerSettingsWindow.cs
@@ -0,0 +1,52 @@
+#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Yumineko.InitializeOnBoot.Editor
+{
+    public class OnBootInitializerSettingsWindow : EditorWindow
+    {
+        private const string MenuPath = "Tools/Yumineko/OnBoot Initializer";
+        private const string EnableKey = "Yumineko.OnBootInitializer.Enable";
+        private static bool _isEnabled;
+
+        [MenuItem(MenuPath)]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<OnBootInitializerSettingsWindow>("OnBoot Initializer");
+            window.Show();
+        }
+
+        private void OnEnable()
+        {
+            _isEnabled = EditorUserSettings.GetConfigValue(EnableKey) != "false";
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.Space(10);
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUILayout.LabelField("OnBoot Initializer Settings", EditorStyles.boldLabel);
+                EditorGUILayout.Space(5);
+
+                using (var check = new EditorGUI.ChangeCheckScope())
+                {
+                    _isEnabled = EditorGUILayout.Toggle("Enable OnBoot Initializer", _isEnabled);
+
+                    if (check.changed)
+                    {
+                        EditorUserSettings.SetConfigValue(EnableKey, _isEnabled.ToString().ToLower());
+                    }
+                }
+
+                EditorGUILayout.Space(5);
+                EditorGUILayout.HelpBox("このオプションを有効にすると、プレイモード開始時に自動的にブートシーン（ビルドインデックス0）をロードして初期化します。", MessageType.Info);
+            }
+        }
+
+        internal static bool IsEnabled() => EditorUserSettings.GetConfigValue(EnableKey) != "false";
+    }
+}
+#endif
