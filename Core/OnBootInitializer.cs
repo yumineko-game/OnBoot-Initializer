366089e7f6a85c4a16eddfb56ff632618a0a6de2
Yumineko <lucky.duet@gmail.com>
2025-03-28 23:21:17 +0900

    RuntimeInitializeのタイミングを調整

--- Core/OnBootInitializer.cs
+++ Core/OnBootInitializer.cs
@@ -10,7 +10,7 @@ namespace Yumineko.InitializeOnBoot
 {
     public static class OnBootInitializer
     {
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void InitializeOnLoad()
         {
             Debug.Log("Initializing OnBoot Initializer");
